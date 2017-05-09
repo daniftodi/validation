9a6199af7209fec92504ac9bc264611670bf4785
Dan Iftodi <daniftodi@gmail.com>
2017-05-09 22:22:00 +0300

    v 0.9.3 published

--- app/src/main/java/com/iftodi/dan/validation/interfaces/PatternValidator.java
+++ app/src/main/java/com/iftodi/dan/validation/interfaces/PatternValidator.java
@@ -1,8 +1,12 @@
 package com.iftodi.dan.validation.interfaces;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by daniftodi on 4/28/16.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PatternValidator {
     String message() default "Invalid input";
     String pattern();
--- app/src/main/java/com/iftodi/dan/validation/interfaces/StringValidator.java
+++ app/src/main/java/com/iftodi/dan/validation/interfaces/StringValidator.java
@@ -1,8 +1,12 @@
 package com.iftodi.dan.validation.interfaces;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by daniftodi on 4/27/16.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface StringValidator {
     String message() default "Length must be between %d and %d";
     int minLength() default -1;
